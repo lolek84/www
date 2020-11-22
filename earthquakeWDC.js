3d03fe85ef6f6c92cfbb140c225fb2621d9d9946
lolek84 <ktrzaska@gmail.com>
2020-11-22 13:43:02 +0100

    Update earthquakeWDC.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -54,11 +54,6 @@
 
     tableau.registerConnector(myConnector);
     
-    $(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "USGS Earthquake Feed";
-        tableau.submit();
-    });
-});
+  
     
 })();
